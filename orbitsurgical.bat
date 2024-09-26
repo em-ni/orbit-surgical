002f0b68969a9443d502cf639bc4115e20d9b8f8
Emanuele-n <z5440219@ad.unsw.edu.au>
2024-09-27 09:59:29 +1000

    windows support

---
+++ orbitsurgical.bat
@@ -0,0 +1,3 @@
+CALL %IsaacLab_PATH%\isaaclab.bat -p -m pip install -e source\extensions\orbit.surgical.ext
+CALL %IsaacLab_PATH%\isaaclab.bat -p -m pip install -e source\extensions\orbit.surgical.assets
+CALL %IsaacLab_PATH%\isaaclab.bat -p -m pip install -e source\extensions\orbit.surgical.tasks
